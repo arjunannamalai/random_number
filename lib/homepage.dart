205c976825793caa3c5db74a368c2f94f75f4f34
Arjun <j.arjunannamalai@gmail.com>
2022-09-23 10:01:42 +0530

    dark theme appbar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:random_number/randomnumber.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -11,164 +11,56 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  TextEditingController textEditingController1 = TextEditingController();
-  TextEditingController textEditingController2 = TextEditingController();
-  String text1 = "1";
-  @override
-  void initState() {
-    super.initState();
-    textEditingController1 = TextEditingController(text: "1");
-    textEditingController2 = TextEditingController(text: "100");
-  }
-
-  int randomNumber(int min, int max) {
-    Random rnd = new Random();
-    int r = min + rnd.nextInt(max - min);
-    return r;
-  }
-
+  bool darkMode = false;
+  Color textcolor = Colors.black;
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          "Random Number Generator",
-          style: TextStyle(color: Colors.black, fontSize: 22),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+          //titleTextStyle: TextStyle(color: Colors.white),
         ),
-        // actions: <Widget>[
-        //   Icon(
-        //     Icons.nightlight,
-        //     color: Colors.black,
-        //   )
-        // ],
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: false,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: TextTheme(),
+        //text
       ),
-      body: SafeArea(
-          child: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                //color: Colors.amber,
-                width: width - 10,
-                height: width / 2,
-                child: FittedBox(
-                    fit: BoxFit.contain,
-                    child: Text(
-                      text1,
-                      style: TextStyle(fontFamily: 'Open_Sans'),
-                    )),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 3,
-                          child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            controller: textEditingController1,
-                            style: TextStyle(color: Colors.black, fontSize: 24),
-                            decoration: InputDecoration(
-                              labelText: "Min",
-                              labelStyle:
-                                  TextStyle(color: Colors.black, fontSize: 24),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide:
-                                    BorderSide(width: 1.5, color: Colors.grey),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide:
-                                    BorderSide(width: 1.5, color: Colors.grey),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Expanded(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 3,
-                          child: TextFormField(
-                            keyboardType: TextInputType.number,
-                            controller: textEditingController2,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 22,
-                            ),
-                            decoration: InputDecoration(
-                              labelText: "Max",
-                              labelStyle:
-                                  TextStyle(color: Colors.black, fontSize: 24),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide:
-                                    BorderSide(width: 1.5, color: Colors.grey),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5),
-                                borderSide:
-                                    BorderSide(width: 1.5, color: Colors.grey),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InkWell(
+      themeMode: darkMode ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Random Number Generator",
+            style: TextStyle(color: textcolor, fontSize: 22),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
                 onTap: () {
                   setState(() {
-                    int min = int.parse(textEditingController1.text);
-                    int max = int.parse(textEditingController2.text);
-                    try {
-                      text1 = (min + Random().nextInt(max - min)).toString();
-                    } catch (e) {
-                      text1 = "1";
-                    }
+                    darkMode = darkMode == true ? false : true;
+                    textcolor =
+                        textcolor == Colors.white ? Colors.black : Colors.white;
                   });
                 },
-                child: Container(
-                  width: width - 10,
-                  height: width * 0.18,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Generate",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ),
+                child: Icon(
+                  Icons.wb_sunny_rounded,
+                  size: 28,
+                  color: Colors.black,
                 ),
               ),
-            ],
-          ),
+            )
+          ],
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: false,
         ),
-      )),
+        body: SafeArea(
+          child: RandomNumber(),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-    return MaterialApp(
-      
-      debugShowCheckedModeBanner: false,
-      home: Homepage(),
-    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return Homepage();
   }
 }
